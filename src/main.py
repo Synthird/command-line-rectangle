a35371b3a96f2a9f2feebea740bae8a0c5c9494f
Synthird <105394020+Synthird@users.noreply.github.com>
2024-12-02 15:04:23 -0800

    Cannot print a rectangle when a zero is entered

--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 width_string: str = ""
 
 
+def display_error():
+    print("Cannot print a rectangle!")
+    print("Probably because you entered:")
+    print("a) Decimals\nb) Letters\nc) Symbols\nd) Spaces between numbers\ne) you typed a zero")
+    raise SystemExit
+
+
 def ask_question(prompt: str):
     print(f"--- What is the {prompt} of the rectangle? ---")
     try:
-        return int(input(f"{prompt.capitalize()}: "))
+        number: int = int(input(f"{prompt.capitalize()}: "))
+        if number > 0:
+            return number
+        else:
+            raise ValueError
     except:
-        print("Cannot print a rectangle!")
-        print("Probably because you entered:")
-        print("a) Decimals\nb) Letters\nc) Symbols\nd) Spaces between numbers")
-        raise SystemExit
+        display_error()
 
 
 width: int = ask_question("width")

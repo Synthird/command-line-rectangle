940ef0ce8c7ed552dffc61a2446bfab1d6787474
Synthird <105394020+Synthird@users.noreply.github.com>
2024-12-02 15:05:23 -0800

    Added static typing to the functions

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 width_string: str = ""
 
 
-def display_error():
+def display_error() -> None:
     print("Cannot print a rectangle!")
     print("Probably because you entered:")
     print("a) Decimals\nb) Letters\nc) Symbols\nd) Spaces between numbers\ne) you typed a zero")
     raise SystemExit
 
 
-def ask_question(prompt: str):
+def ask_question(prompt: str) -> str:
     print(f"--- What is the {prompt} of the rectangle? ---")
     try:
         number: int = int(input(f"{prompt.capitalize()}: "))

a2d2e14a573fd47e9c1abc3725a719ca40b1cac9
Synthird <105394020+Synthird@users.noreply.github.com>
2024-12-02 16:12:15 -0800

    Removed the display_error function since it is not used anywhere else

--- src/main.py
+++ src/main.py
@@ -1,17 +1,6 @@
 width_string: str = ""
 
 
-def display_error() -> None:
-    print("!!! Cannot print a rectangle! !!!")
-    print("Probably because you entered:")
-    print("a) Decimals")
-    print("b) Letters")
-    print("c) Symbols")
-    print("d) Spaces between numbers")
-    print("e) A zero")
-    raise SystemExit
-
-
 def ask_question(prompt: str) -> int:
     print(f"--- What is the {prompt} of the rectangle? ---")
     try:
@@ -22,7 +11,14 @@ def ask_question(prompt: str) -> int:
         else:
             raise ValueError
     except:
-        display_error()
+        print("!!! Cannot print a rectangle! !!!")
+        print("Probably because you entered:")
+        print("a) Decimals")
+        print("b) Letters")
+        print("c) Symbols")
+        print("d) Spaces between numbers")
+        print("e) A zero")
+        raise SystemExit
 
 
 width: int = ask_question("width")

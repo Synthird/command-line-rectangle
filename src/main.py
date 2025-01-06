aa673ad93642f457d19d16be682212d044f34290
Synthird <105394020+Synthird@users.noreply.github.com>
2025-01-05 21:37:05 -0800

    Removed the exit with reasons function and shortened the code in the ask_length_of function

--- src/main.py
+++ src/main.py
@@ -1,31 +1,20 @@
 width_string: str = ""
 
 
-def exit_with_possible_reasons() -> None:
-	print("!!! Cannot print dots! !!!")
-	print("Probably because you entered:")
-	print("a) Decimals")
-	print("b) Letters")
-	print("c) Symbols")
-	print("d) A negative number")
-	print("e) You exited the program")
-	raise SystemExit
-
-
 def ask_length_of(length: str) -> int:
-	number: int = 0
-
 	print(f"--- What is the {length} of the rectangle? ---")
 
 	try:
-		number = int(input(f"{length.capitalize()}: ").replace(" ", ""))
+		return int(input(f"{length.capitalize()}: ").replace(" ", ""))
 	except:
-		exit_with_possible_reasons()
-
-	if number > -1:
-		return number
-	else:
-		exit_with_possible_reasons()
+		print("!!! Cannot print dots! !!!")
+		print("Probably because you entered:")
+		print("a) Decimals")
+		print("b) Letters")
+		print("c) Symbols")
+		print("d) A negative number")
+		print("e) You exited the program")
+		raise SystemExit
 
 
 width: int = ask_length_of("width")
